10ef7aaa257975bf7cff8c58bce1406e58cbbd7e
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-07-23 17:35:18 +0530

    Genereate employee id

--- src/controller/ManageEmployeeController.java
+++ src/controller/ManageEmployeeController.java
@@ -1,19 +1,48 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import db.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
+import java.sql.*;
+
 public class ManageEmployeeController {
     public AnchorPane root;
     public TableView tblEmployee;
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtSalary;
+    public JFXTextField txtETF;
 
-    public void btnNewEmployee_OnAction(ActionEvent actionEvent) {
-        generateNewEmployeeID();
+    public void initialize(){
+        txtId.setEditable(false);
+        txtETF.setEditable(false);
     }
 
-    private void generateNewEmployeeID() {
+    public void btnNewEmployee_OnAction(ActionEvent actionEvent) {
+        try {
+            generateNewEmployeeID();
+            txtName.clear();
+            txtSalary.clear();
+            txtName.requestFocus();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 
+    private void generateNewEmployeeID() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Employee ORDER BY id DESC LIMIT 1");
+        String newEmployeeId = "E001";
+        if (rst.next()) {
+            String lastEmployeeId = rst.getString(1);
+            newEmployeeId = "E" + (Integer.parseInt(lastEmployeeId.substring(1)) + 1);
+        }
+        txtId.setText(newEmployeeId);
+        connection.close();
     }
 
     public void txtId(ActionEvent actionEvent) {
