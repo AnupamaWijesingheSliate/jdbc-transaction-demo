9630247fc78cb3a9aa57d27f4a1b05c9003cb8a1
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-07-23 18:09:15 +0530

    Generate Employee ID using a String formatter

--- src/controller/ManageEmployeeController.java
+++ src/controller/ManageEmployeeController.java
@@ -1,12 +1,20 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import db.DBConnection;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
-import java.sql.*;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Formatter;
 
 public class ManageEmployeeController {
     public AnchorPane root;
@@ -15,18 +23,47 @@ public class ManageEmployeeController {
     public JFXTextField txtName;
     public JFXTextField txtSalary;
     public JFXTextField txtETF;
+    public JFXButton btnNewEmployee;
 
-    public void initialize(){
+    public void initialize() {
         txtId.setEditable(false);
         txtETF.setEditable(false);
+        reset(true, true);
+        txtSalary.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (newValue.matches("[0-9]+[.]?[0-9]*")) {
+                    txtETF.setText(new BigDecimal(newValue).multiply(new BigDecimal(10)).divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_DOWN).toPlainString() + "%");
+                    txtSalary.setPromptText("Employee Salary");
+                } else {
+                    txtETF.clear();
+                    txtSalary.setPromptText("Invalid Salary");
+                }
+            }
+        });
+    }
+
+    private void reset(boolean clearId, boolean focusToNewEmployee) {
+        txtName.setDisable(false);
+        txtSalary.setDisable(false);
+        if (clearId) {
+            txtId.clear();
+            txtName.setDisable(true);
+            txtSalary.setDisable(true);
+        }
+        txtName.clear();
+        txtSalary.clear();
+        txtETF.clear();
+        tblEmployee.getSelectionModel().clearSelection();
+        if (focusToNewEmployee) {
+            btnNewEmployee.requestFocus();
+        }
     }
 
     public void btnNewEmployee_OnAction(ActionEvent actionEvent) {
         try {
             generateNewEmployeeID();
-            txtName.clear();
-            txtSalary.clear();
-            txtName.requestFocus();
+            reset(false, false);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -39,7 +76,7 @@ public class ManageEmployeeController {
         String newEmployeeId = "E001";
         if (rst.next()) {
             String lastEmployeeId = rst.getString(1);
-            newEmployeeId = "E" + (Integer.parseInt(lastEmployeeId.substring(1)) + 1);
+            newEmployeeId = String.format("E%03d" , (Integer.parseInt(lastEmployeeId.substring(1)) + 1));
         }
         txtId.setText(newEmployeeId);
         connection.close();
