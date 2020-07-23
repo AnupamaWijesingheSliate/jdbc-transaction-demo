115ab7a756f0fd5be1297eaaf9ab53c8e83c58e7
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-07-23 17:23:02 +0530

    Add ManageEmployeeController class

---
+++ src/controller/ManageEmployeeController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class ManageEmployeeController {
+    public AnchorPane root;
+    public TableView tblEmployee;
+
+    public void btnNewEmployee_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtId(ActionEvent actionEvent) {
+    }
+
+    public void txtName(ActionEvent actionEvent) {
+    }
+
+    public void txtSalary(ActionEvent actionEvent) {
+    }
+
+    public void txtETF(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+}
